e08fb8717d47124ac1bd1c691da22f8a1cc5300d
shanzaayub <shanzaayub97@gmail.com>
2024-05-30 12:32:49 -0400

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hmiVAE",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/camlab-bioml/hmiVAE",
     project_urls={
         "Issues": "https://github.com/camlab-bioml/hmiVAE/issues",
